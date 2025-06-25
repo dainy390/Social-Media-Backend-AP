ce380ca16d836e1201c96b554375ee6ae8ad86ad
Dainy chaudhary <112776500+dainy390@users.noreply.github.com>
2025-06-25 09:29:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-import app from "./index.js";
-import { connectToDb } from "./src/config/db.js";
+// import app from "./index.js";
+// import { connectToDb } from "./src/config/db.js";
 
-const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, async () => {
-  await connectToDb();
-  console.log(`Server is running on port ${PORT}`);
-});
+// app.listen(PORT, async () => {
+//   await connectToDb();
+//   console.log(`Server is running on port ${PORT}`);
+// });
