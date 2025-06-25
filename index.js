610860e9f8ade64f1639ece638b4c54bf11d0107
Dainy chaudhary <112776500+dainy390@users.noreply.github.com>
2025-06-25 09:27:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 import dotenv from "dotenv";
 dotenv.config();
+
 import express from "express";
+import cookieParser from "cookie-parser";
+
 import userRouter from "./src/features/users/user.routes.js";
 import postRouter from "./src/features/Posts/post.routes.js";
 import commentRouter from "./src/features/Comments/comment.routes.js";
 import likeRouter from "./src/features/Likes/like.routes.js";
 import friendshipRouter from "./src/features/Friendship/friendship.routes.js";
 import otpRouter from "./src/features/OTP/otp.routes.js";
-import cookieParser from "cookie-parser";
 import { appLevelErrorHandlerMiddleware } from "./src/middlewares/errorHandler.js";
-
-
-dotenv.config();
 
 const app = express();
 
 app.use(cookieParser());
 app.use(express.json());
+
 app.use("/api/users", userRouter);
 app.use("/api/posts", postRouter);
 app.use("/api/comments", commentRouter);
@@ -24,7 +24,7 @@ app.use("/api/likes", likeRouter);
 app.use("/api/friends", friendshipRouter);
 app.use("/api/otp", otpRouter);
 
-
+// Global error handler
 app.use(appLevelErrorHandlerMiddleware);
 
 export default app;
